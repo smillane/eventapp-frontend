0451696b250664c8445515ce8582b184fe3d147d
Sean Millane <jmillaneiv@gmail.com>
2021-04-23 01:47:38 -0400

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Event app to find local events, from concerts, to any others</p>
+      <p>version 1.0.0</p>
+    </div>
+  )
+}
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+export default function AddEventPage() {
+  return (
+    <div>
+      <h1>add event</h1>
+    </div>
+  )
+}
