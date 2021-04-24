fdebe90ca105a4598b006468e3a4d0f2fe5b06b6
Sean Millane <jmillaneiv@gmail.com>
2021-04-24 00:06:08 -0400

    mapping data, homepage design, styling, stock photos for example data, designing event display on homepage

---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to EventsApp</h1>
+      <h2>Find whatever type of event you're looking for</h2>
+    </div>
+  )
+}
